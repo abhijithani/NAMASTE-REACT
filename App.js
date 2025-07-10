b6364515d65afe5e8de0a4cf8b9f585eee74ec21
abhijithani <abhijithmscr7@gmail.com>
2025-07-10 09:33:22 +0530

    lect 2 added react

--- App.js
+++ App.js
@@ -1,28 +1,33 @@
-/*const heading = React.createElement
-("h1", {id:"heading" }, "hello world from react");
 
-console.log(heading);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-
-root.render(heading);*/
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+ 
 const Parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" },
-        React.createElement("div", { id: "parent" }, [
-            React.createElement("h1", {}, "i am h1 heading"),
-            React.createElement("h2", {}, "i am h2 heading")]
-
-        )),
-    React.createElement("div", { id: "child" },
-        React.createElement("div", { id: "parent" }, [
-            React.createElement("h1", {}, "i am h1 heading"),
-            React.createElement("h2", {}, "i am h2 heading")]
-
-        ))]
-);
+  React.createElement(
+    "div",
+    { id: "child", key: "child-1" },
+    React.createElement(
+      "div",
+      { id: "parent" },
+      [
+        React.createElement("h1", { key: "h1-1" }, "i am h1 heading"),
+        React.createElement("h2", { key: "h2-1" }, "i am h2 heading"),
+      ]
+    )
+  ),
+  React.createElement(
+    "div",
+    { id: "child", key: "child-2" },
+    React.createElement(
+      "div",
+      { id: "parent" },
+      [
+        React.createElement("h1", { key: "h1-2" }, "i am h1 heading"),
+        React.createElement("h2", { key: "h2-2" }, "i am h2 heading"),
+      ]
+    )
+  )
+]);
 console.log(Parent)
 
  
---
+++ README.md
@@ -0,0 +1,22 @@
+# namaste react
+
+# parcel
+- dev build 
+- local server
+- HMR = hot Module replacement
+- File  Watching Algorithm -written in c++
+- Caching - faster Builds
+- Image optmization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code splitting
+- Differential bundling -  support older browser
+- Diagnostic
+- Error handling
+- HTTPs
+- Tree shaking = remove unuseful function
+- Different dev and prod bundles
+
+
