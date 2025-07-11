12723535c158f6a2b9c9af5fa7501b3dda16ee01
abhijithani <abhijithmscr7@gmail.com>
2025-07-11 10:22:58 +0530

    ep03 leaned component composition

--- App.js
+++ App.js
@@ -1,36 +1,26 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
- 
-const Parent = React.createElement("div", { id: "parent" }, [
-  React.createElement(
-    "div",
-    { id: "child", key: "child-1" },
-    React.createElement(
-      "div",
-      { id: "parent" },
-      [
-        React.createElement("h1", { key: "h1-1" }, "i am h1 heading"),
-        React.createElement("h2", { key: "h2-1" }, "i am h2 heading"),
-      ]
-    )
-  ),
-  React.createElement(
-    "div",
-    { id: "child", key: "child-2" },
-    React.createElement(
-      "div",
-      { id: "parent" },
-      [
-        React.createElement("h1", { key: "h1-2" }, "i am h1 heading"),
-        React.createElement("h2", { key: "h2-2" }, "i am h2 heading"),
-      ]
-    )
-  )
-]);
-console.log(Parent)
 
- 
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React from jsx
+  </h1>
+);
+
+
+//component composition
+const HeadingComponent = () => (
+  <div id="Container">
+    {Title()}
+    <Title></Title>
+    <Title />
+    <h1> Namaste React from functional component
+    </h1>
+  </div>
+);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(Parent);
+root.render(<HeadingComponent />);
