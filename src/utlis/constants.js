0d048fc721cd692fced242e4c856edc39c90a8be
abhijithani <abhijithmscr7@gmail.com>
2025-07-18 13:20:08 +0530

    added cors links to the apis

--- src/utlis/constants.js
+++ src/utlis/constants.js
@@ -1,5 +1,5 @@
 export const LOGO_URL ="https://cbx-prod.b-cdn.net/COLOURBOX38603643.jpg?width=800&height=800&quality=70";
-export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+export const IMG_CDN_URL = "https://corsproxy.io/?url=https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
  
-export const RESMENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=9.9312328&lng=76.26730409999999&restaurantId="
-export const RESMENU_IMG = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/"
+export const RESMENU_URL = "https://corsproxy.io/?url=https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=9.9312328&lng=76.26730409999999&restaurantId="
+export const RESMENU_IMG = "https://corsproxy.io/?url=https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_600/"
